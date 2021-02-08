f679ca229dbece9e637f76b791130181e4ae1e3b
nicoluv <55165208+nicoluv@users.noreply.github.com>
2021-02-07 23:45:13 -0400

    funcion quita \t \n

--- Proyecto/src/Codigo/Funciones.java
+++ Proyecto/src/Codigo/Funciones.java
@@ -1,8 +1,21 @@
-package Codigo;
+ridsdgfdggg package Codigo;
+import java.io.File;
 
 public class Funciones {
 	
 	
-	//confirmando 
+	
 
+    static public StringBuilder quitarEspacios(File miArchivo) throws FileNotFoundException, IOException {
+
+        BufferedReader br = new BufferedReader(new FileReader(miArchivo));
+        String linea;
+        StringBuilder sb = new StringBuilder();
+
+        while ((linea = br.readLine()) != null) {
+            sb.append(linea.trim());
+        }
+        return sb;
+    }
+    
 }
